33e745fd7f9dc98655cdd5d35af2edb35e652ebe
nayoseb <nayoseb1018@gmail.com>
2023-04-20 22:57:27 +0900

    initial commit

---
+++ src/main/java/com/YYduo/KkuldongVarietyStore/KkuldongVarietyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.YYduo.KkuldongVarietyStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkuldongVarietyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkuldongVarietyStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YYduo/KkuldongVarietyStore/KkuldongVarietyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YYduo.KkuldongVarietyStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkuldongVarietyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
